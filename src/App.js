445c9a80268336a1437ad21e70b7429752caf1e9
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-12-06 20:10:56 +0800

    added react-router

--- src/App.js
+++ src/App.js
@@ -5,18 +5,6 @@ import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 
 function App() {
-	useEffect(() => {
-		// https://imdb-api.com/en/API/Title/k_w5557402/tt1375666/FullActor,FullCast,Posters,Images,Trailer,Ratings,
-		const fetchMovie = async () => {
-			const response = await fetch(
-				'https://imdb-api.com/en/API/Title/k_w5557402/tt0110413'
-			);
-			const movie = await response.json();
-			console.log(movie);
-		};
-		fetchMovie();
-	}, []);
-
 	return (
 		<div className="bg-primary text-neutral h-screen font-serif">
 			<Header />
