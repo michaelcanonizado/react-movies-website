7a9dde59a771d2c65d2ccd7451e2e641e929e6ae
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-12-11 01:39:51 +0800

    Added force Full HD 1080p to trailer embbed

--- src/components/Main/MovieDetails/MovieTrailer.js
+++ src/components/Main/MovieDetails/MovieTrailer.js
@@ -1,13 +1,4 @@
 import React from 'react';
-import YouTube from 'react-youtube';
-
-const opts = {
-	height: '390',
-	width: '640',
-	playerVars: {
-		autoplay: 0,
-	},
-};
 
 export default function MovieTrailer({ trailer, movieId }) {
 	return (
@@ -19,22 +10,10 @@ export default function MovieTrailer({ trailer, movieId }) {
 				height: '0',
 			}}
 		>
-			{/* <YouTube
-				videoId={trailer.youtubeId}
-				opts={opts}
-				style={{
-					position: 'absolute',
-					top: '0',
-					left: '0',
-					width: '100%',
-					height: '100%',
-				}}
-			/> */}
-			{/* &origin=https://localhost:3000/movie/${movieId} */}
 			<iframe
 				width="560"
 				height="315"
-				src={`https://www.youtube.com/embed/${trailer.youtubeId}`}
+				src={`https://www.youtube.com/embed/${trailer.youtubeId}?vq=hd1080`}
 				title="YouTube video player"
 				frameBorder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
