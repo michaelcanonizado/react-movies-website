ad17cf2a4f2c360c897ce6245816d306963efb88
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-12-16 02:03:23 +0800

    Added h-fit to poster component to prefent from stretching when collpased description expands

--- src/components/Main/MovieDetails/MoviePoster.js
+++ src/components/Main/MovieDetails/MoviePoster.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MoviePoster({ link, title, className }) {
 	return (
-		<div className={`${className} aspect-poster`}>
+		<div className={`${className} aspect-poster h-fit`}>
 			<img className="h-full object-cover" src={link} alt={title} />
 		</div>
 	);
